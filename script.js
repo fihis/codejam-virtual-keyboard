261dcf2c186b3d27bc160a6516a725504792bfd6
Fihis <isifuo@gmail.com>
2019-11-06 22:29:26 +0300

    fix mouse drag issue

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-//stopimmidiatepropagation - предотвращает выполнение последующих ивентов
-//стоппропагейшн - останавливает всплытие, но выполнение других ивентов продолжается
-//повесить ивент листенер кейдаун на инпут и сделать ивент.превентдефолт 
-
-//если е.таргет !=инпут то обработка как обычно
-
-
 window.onload = function () {
     console.log(localStorage.getItem('language'));
     if (localStorage.getItem('language') === null) {
         localStorage.setItem('language', 'en');
         console.log(localStorage.getItem('language'));
     }
-    //localStorage.setItem('language', 'en');
     let lang = localStorage.getItem('language');
     document.querySelector('body').style.display = 'flex';
     document.querySelector('body').style.flexDirection = 'column';
@@ -23,12 +15,9 @@ window.onload = function () {
     input.style.fontSize = '20px';
     document.body.append(input);
     input.readOnly = true;
-
     let keyboard = document.createElement('div');
     keyboard.className = "keyboard";
     keyboard.style.display = 'flex';
-    
-    
     document.body.append(keyboard);
     let firstRow = [
         ['Backquote', '`', '~', 'ё', 'Ё'],
@@ -45,7 +34,6 @@ window.onload = function () {
         ['Minus', '-', '_', '-', '_'],
         ['Equal', '=', '+', '=', '+'],
         ['Backspace', 'Backspace', 'Backspace', 'Backspace', 'Backspace']];
-        
     let secondRow = [
         ['Tab', 'Tab', 'Tab', 'Tab', 'Tab'],
         ['KeyQ', 'q', 'Q', 'й', 'Й'],
@@ -62,7 +50,6 @@ window.onload = function () {
         ['BracketRight', ']', '}', 'ъ', 'Ъ'],
         ['Delete', 'Delete', 'Delete', 'Delete', 'Delete']
     ];
-
     let thirdRow = [
         ['CapsLock', 'CapsLock', 'CapsLock', 'CapsLock', 'CapsLock'],
         ['KeyA', 'a', 'A', 'ф', 'Ф'],
@@ -91,7 +78,7 @@ window.onload = function () {
         ['Comma', ',', '<', 'б', 'Б'],
         ['Period', '.', '>', 'ю', 'Ю'],
         ['Slash', '/', '?', '.', ','],
-        ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift']       
+        ['ShiftRight', 'Shift', 'Shift', 'Shift', 'Shift']
     ];
     let fifthRow = [
         ['ControlLeft', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
@@ -100,59 +87,53 @@ window.onload = function () {
         ['AltRight', 'Alt', 'Alt', 'Alt', 'Alt'],
         ['ControlRight', 'Ctrl', 'Ctrl', 'Ctrl', 'Ctrl'],
     ]
-    
-  
     let row = []
-    for (let i = 0; i < 5; i++){    
+    for (let i = 0; i < 5; i++) {
         row[i] = document.createElement('div');
         row[i].className = "row";
         row[i].style.display = 'flex';
         row[i].style.justifyContent = 'center';
         document.body.append(row[i]);
     }
-
     let firstRowKeys = [];
     let secondRowKeys = [];
     let thirdRowKeys = [];
     let fourthRowKeys = [];
     let fifthRowKeys = [];
-
+    //класс "кнопка"
     class Key {
-        constructor (name, enSpan, enShiftSpan, ruSpan, ruShiftSpan, parent){
+        constructor(name, enSpan, enShiftSpan, ruSpan, ruShiftSpan, parent) {
             this.name = name;
             this.parent = parent;
             this.enSpanValue = enSpan;
             this.enShiftSpanValue = enShiftSpan;
             this.ruSpanValue = ruSpan;
             this.ruShiftSpanValue = ruShiftSpan;
-
             this.createKey = () => {
                 this.div = document.createElement('div');
                 this.div.className = 'key';
                 this.div.id = name;
                 this.parent.append(this.div);
-    
                 this.enSpan = document.createElement('span');
                 this.enSpan.innerHTML = this.enSpanValue;
                 if (lang == 'en') {
-                this.enSpan.className = 'show';
-                this.curSpan = enSpan;}
-                else {this.enSpan.className = 'hide';}
+                    this.enSpan.className = 'show';
+                    this.curSpan = enSpan;
+                }
+                else { this.enSpan.className = 'hide'; }
                 this.div.append(this.enSpan);
-
                 this.enShiftSpan = document.createElement('span');
                 this.enShiftSpan.innerHTML = this.enShiftSpanValue;
                 this.enShiftSpan.className = 'hide';
                 this.div.append(this.enShiftSpan);
-
                 this.ruSpan = document.createElement('span');
                 this.ruSpan.innerHTML = this.ruSpanValue;
                 if (lang == 'ru') {
                     this.ruSpan.className = 'show';
-                    this.curSpan = ruSpan;}
-                    else {this.ruSpan.className = 'hide';}
+                    this.curSpan = ruSpan;
+                }
+                else { this.ruSpan.className = 'hide'; }
                 this.div.append(this.ruSpan);
-
                 this.ruShiftSpan = document.createElement('span');
                 this.ruShiftSpan.innerHTML = this.ruShiftSpanValue;
                 this.ruShiftSpan.className = 'hide';
@@ -166,21 +147,18 @@ window.onload = function () {
                     this.curSpan = ruSpan;
                     localStorage.setItem('language', 'ru');
                     lang = 'ru';
-
                 } else if (this.ruSpan.className == 'show') {
                     this.ruSpan.className = 'hide';
                     this.enSpan.className = 'show';
                     this.curSpan = enSpan;
                     localStorage.setItem('language', 'en');
                     lang = 'en';
-
                 } else if (this.enShiftSpan.className == 'show') {
                     this.enShiftSpan.className = 'hide';
                     this.ruShiftSpan.className = 'show';
                     this.curSpan = ruShiftSpan;
                     localStorage.setItem('language', 'ru');
                     lang = 'ru';
-
                 } else if (this.ruShiftSpan.className == 'show') {
                     this.ruShiftSpan.className = 'hide';
                     this.enShiftSpan.className = 'show';
@@ -190,8 +168,6 @@ window.onload = function () {
                 }
                 console.log(lang);
             }
-                
-            
             this.switchCase = () => {
                 if (lang == 'en') {
                     if (this.enSpan.className == 'show') {
@@ -202,247 +178,186 @@ window.onload = function () {
                         this.enSpan.className = 'show';
                         this.curSpan = enSpan;
                         this.enShiftSpan.className = 'hide';
-                }
-            }   else {
-
-                
-                
-                if (this.ruSpan.className == 'show') {
-                    this.ruSpan.className = 'hide';
-                    this.ruShiftSpan.className = 'show';
-                    this.curSpan = ruShiftSpan;
+                    }
                 } else {
-                    this.ruSpan.className = 'show';
-                    this.curSpan = ruSpan;
-                    this.ruShiftSpan.className = 'hide';
+                    if (this.ruSpan.className == 'show') {
+                        this.ruSpan.className = 'hide';
+                        this.ruShiftSpan.className = 'show';
+                        this.curSpan = ruShiftSpan;
+                    } else {
+                        this.ruSpan.className = 'show';
+                        this.curSpan = ruSpan;
+                        this.ruShiftSpan.className = 'hide';
+                    }
                 }
             }
-            }
-            
-
         }
     }
-    
+    //первый ряд
     for (let i = 0; i < firstRow.length; i++) {
-        firstRowKeys[i] = new Key(firstRow[i][0],firstRow[i][1],firstRow[i][2],firstRow[i][3],firstRow[i][4],row[0]);
-        firstRowKeys[i].createKey();      
+        firstRowKeys[i] = new Key(firstRow[i][0], firstRow[i][1], firstRow[i][2], firstRow[i][3], firstRow[i][4], row[0]);
+        firstRowKeys[i].createKey();
     }
     firstRowKeys[firstRow.length - 1].div.style.width = '100px';
     firstRowKeys[firstRow.length - 1].div.style.fontSize = '16px';
-
+    //второй ряд
     for (let i = 0; i < secondRow.length; i++) {
-        secondRowKeys[i] = new Key(secondRow[i][0],secondRow[i][1],secondRow[i][2],secondRow[i][3],secondRow[i][4],row[1]);
-        secondRowKeys[i].createKey();      
+        secondRowKeys[i] = new Key(secondRow[i][0], secondRow[i][1], secondRow[i][2], secondRow[i][3], secondRow[i][4], row[1]);
+        secondRowKeys[i].createKey();
     }
     secondRowKeys[0].div.style.width = '75px';
     secondRowKeys[0].div.style.fontSize = '24px';
     secondRowKeys[secondRow.length - 1].div.style.width = '75px';
     secondRowKeys[secondRow.length - 1].div.style.fontSize = '16px';
-    
+    //третий ряд
     for (let i = 0; i < thirdRow.length; i++) {
-        thirdRowKeys[i] = new Key(thirdRow[i][0],thirdRow[i][1],thirdRow[i][2],thirdRow[i][3],thirdRow[i][4],row[2]);
-        thirdRowKeys[i].createKey();      
+        thirdRowKeys[i] = new Key(thirdRow[i][0], thirdRow[i][1], thirdRow[i][2], thirdRow[i][3], thirdRow[i][4], row[2]);
+        thirdRowKeys[i].createKey();
     }
     thirdRowKeys[0].div.style.width = '75px';
     thirdRowKeys[0].div.style.fontSize = '12px';
     thirdRowKeys[thirdRow.length - 1].div.style.width = '75px';
     thirdRowKeys[thirdRow.length - 1].div.style.fontSize = '16px';
-    
+    //четвертый ряд
     for (let i = 0; i < fourthRow.length; i++) {
-        fourthRowKeys[i] = new Key(fourthRow[i][0],fourthRow[i][1],fourthRow[i][2],fourthRow[i][3],fourthRow[i][4],row[3]);
-        fourthRowKeys[i].createKey();      
+        fourthRowKeys[i] = new Key(fourthRow[i][0], fourthRow[i][1], fourthRow[i][2], fourthRow[i][3], fourthRow[i][4], row[3]);
+        fourthRowKeys[i].createKey();
     }
     fourthRowKeys[0].div.style.width = '130px';
     fourthRowKeys[0].div.style.fontSize = '20px';
     fourthRowKeys[fourthRow.length - 1].div.style.width = '130px';
     fourthRowKeys[fourthRow.length - 1].div.style.fontSize = '20px';
-
+    //пятый ряд
     for (let i = 0; i < fifthRow.length; i++) {
-        fifthRowKeys[i] = new Key(fifthRow[i][0],fifthRow[i][1],fifthRow[i][2],fifthRow[i][3],fifthRow[i][4],row[4]);
+        fifthRowKeys[i] = new Key(fifthRow[i][0], fifthRow[i][1], fifthRow[i][2], fifthRow[i][3], fifthRow[i][4], row[4]);
         fifthRowKeys[i].createKey();
         fifthRowKeys[i].div.style.width = '75px';
         fifthRowKeys[i].div.style.fontSize = '16px';
     }
-    fifthRowKeys[2].div.style.width= '490px';
-    
-
+    fifthRowKeys[2].div.style.width = '490px';
     let allRowKeys = [];
     allRowKeys = firstRowKeys.concat(secondRowKeys, thirdRowKeys, fourthRowKeys, fifthRowKeys);
-
-
-    let pressed = new Set();
-
+    //обработка нажатий
     let shiftDown = false;
     let capsOn = false;
-
-    // function switchLang(e) {
-           
-    //     pressed.add(e.code);
-    //     console.log(pressed);
-    //     for (let code of ['LeftShift', 'LeftAlt']) { // все ли клавиши из набора нажаты?
-    //         console.log('перебор '+code);
-    //         if (!pressed.has(code)) {
-    //           return;
-    //         }
-    //       }
-    //       pressed.clear();
-
-    //       allRowKeys.forEach( (x) => {
-    //           x.switchLanguage();
-    //           console.log('switched!')
-    //       }
-    //       )
-
-    //     };Q
-
-
     document.addEventListener('keydown', (e) => {
-        console.log(e.code); 
-        
-        //console.log(e); 
-        
-        // switchLang(e);
-        
-
-
+        console.log(e.code);
         allRowKeys.forEach((x) => {
-            
             if (e.code === x.name) {
-                
-                if (e.code === 'CapsLock') {                    
+                if (e.code === 'CapsLock') {
                     if (!capsOn) {
                         x.div.style.background = 'blue';
                         x.div.style.transform = 'scale(0.9)';
                         capsOn = true;
-                        for (let i = 13; i < allRowKeys.length; i++){
+                        for (let i = 13; i < allRowKeys.length; i++) {
                             this.console.log()
                             allRowKeys[i].switchCase();
                         }
-                    }  else
-                    if (capsOn){
-                        x.div.style.background = 'black';
-                        x.div.style.transform = 'scale(1)';
-                        capsOn = false;
-                        for (let i = 13; i < allRowKeys.length; i++){
-                            this.console.log()
-                            allRowKeys[i].switchCase();
+                    } else
+                        if (capsOn) {
+                            x.div.style.background = 'black';
+                            x.div.style.transform = 'scale(1)';
+                            capsOn = false;
+                            for (let i = 13; i < allRowKeys.length; i++) {
+                                this.console.log()
+                                allRowKeys[i].switchCase();
+                            }
                         }
-                    }
                     return;
-                }            
+                }
                 x.div.style.background = 'blue';
                 x.div.style.transform = 'scale(0.9)';
-
-                if ((e.altKey) && (e.shiftKey)){
-                    
+                if ((e.altKey) && (e.shiftKey)) {
                     allRowKeys.forEach((x) => {
                         x.switchLanguage();
                     })
-                    
                 }
-
                 if (e.code === 'Tab') {
                     e.preventDefault();
                     input.value = input.value + '\t';
                 }
                 else
-                if (e.code === 'Enter') {
-                    e.preventDefault();
-                    input.value = input.value + '\n';
-                }
-                else 
-                if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')) {
-                    if (!shiftDown) {
-                        shiftDown = true;
-                        allRowKeys.forEach((x) => {
-                            x.switchCase();
-                        })
+                    if (e.code === 'Enter') {
+                        e.preventDefault();
+                        input.value = input.value + '\n';
                     }
-                }  
-                else 
-                if (e.code === 'Delete'){
-                    e.preventDefault();
-                    input.value = input.value.substr(1, input.value.length);
-                    
-                }  
-                else 
-                if (e.code === 'Backspace'){
-                    e.preventDefault();
-                    input.value = input.value.substr(0, input.value.length - 1);                  
-                }  
-                else 
-                if ((e.code === 'ControlLeft') || (e.code === 'ControlRight')){
-                    e.preventDefault();
-                }
-                else 
-                if ((e.code === 'AltLeft') || (e.code === 'AltRight')){
-                    e.preventDefault();
-                }
-                else if (e.target != input) {
-                input.value = input.value + x.curSpan;
-                //console.log(x.curSpan);
-            }         
+                    else
+                        if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')) {
+                            if (!shiftDown) {
+                                shiftDown = true;
+                                allRowKeys.forEach((x) => {
+                                    x.switchCase();
+                                })
+                            }
+                        }
+                        else
+                            if (e.code === 'Delete') {
+                                e.preventDefault();
+                                input.value = input.value.substr(1, input.value.length);
+                            }
+                            else
+                                if (e.code === 'Backspace') {
+                                    e.preventDefault();
+                                    input.value = input.value.substr(0, input.value.length - 1);
+                                }
+                                else
+                                    if ((e.code === 'ControlLeft') || (e.code === 'ControlRight')) {
+                                        e.preventDefault();
+                                    }
+                                    else
+                                        if ((e.code === 'AltLeft') || (e.code === 'AltRight')) {
+                                            e.preventDefault();
+                                        }
+                                        else if (e.target != input) {
+                                            input.value = input.value + x.curSpan;
+                                        }
+            }
         }
-    }
         )
     }
     )
-
-
-
     document.addEventListener('keyup', (e) => {
-        pressed.delete(e.code);
-        //console.log(pressed);
         allRowKeys.forEach((x) => {
             if (e.code === x.name) {
                 if (e.code != 'CapsLock') {
                     x.div.style.background = 'black';
                     x.div.style.transform = 'scale(1)';
-                    if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')){
+                    if ((e.code === 'ShiftLeft') || (e.code === 'ShiftRight')) {
                         shiftDown = false;
                         allRowKeys.forEach((x) => {
-                        x.switchCase();
+                            x.switchCase();
                         })
                     }
                 }
-                
-                
-            }    
+            }
         }
         )
     }
     )
-    
+    //подсветка кнопок при наведении
+    document.addEventListener('mouseover', (e) => {
+        if (e.target.className === 'key') {
+            e.target.style.background = 'rgb(48, 48, 48)';
+        }
+    }
+    )
+    document.addEventListener('mouseout', (e) => {
+        if (e.target.className === 'key') {
+            e.target.style.background = 'black';
+        }
+    }
+    )
+    //обработка мыши
+    let clickTarget = '';
     document.addEventListener('mousedown', (e) => {
         let event = new Event('keydown');
-        //console.log(e.target.id);
         event.code = e.target.id;
+        clickTarget = e.target.id;
         document.dispatchEvent(event);
-        // if (e.target.className === 'key') {
-        //     e.target.style.background = 'red';
-        //     e.target.style.transform = 'scale(0.9)';
-        //     console.log(e.target);
-            
-        //     input.value = input.value + e.target.getElementsByClassName('show')[0].innerHTML;
-        
-        // }
-        
-        
     })
-
-     document.addEventListener('mouseup', (e) => {
+    document.addEventListener('mouseup', (e) => {
         let event = new Event('keyup');
-        //console.log(e.target.id);
-        event.code = e.target.id;
+        event.code = clickTarget;
         document.dispatchEvent(event);
-
-    //     if  (e.target.className === 'key') {
-    //         e.target.style.background = 'black';
-    //         e.target.style.transform = 'scale(1)';
-    //     }
-        
-     })
-
-   
-    
+    })
 }
